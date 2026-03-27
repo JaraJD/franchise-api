7c401cd14e19112482ee6777a5bd9e7df4c1a21c
Juan Jaramillo <salazarjdj@gmail.com>
2026-03-26 23:54:30 -0500

    Initial structure

---
+++ applications/app-service/src/main/java/co/com/jara/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.jara.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.jara.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
